227de1f11bc72f5907492ba43c3400a1498af861
veyn0 <veynoplugins@gmail.com>
2026-02-03 23:18:30 +0100

    1

---
+++ src/main/java/dev/veyno/aiFoliaTPA/AiFoliaTPA.java
@@ -0,0 +1,17 @@
+package dev.veyno.aiFoliaTPA;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AiFoliaTPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
